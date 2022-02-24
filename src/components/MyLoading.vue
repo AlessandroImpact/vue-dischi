f31257d5e6f7eb8c71138a5498482e432ccfba05
Alessandro Terracciano <terracciano41@gmail.com>
2022-02-24 16:22:02 +0100

    bonus loading section

---
+++ src/components/MyLoading.vue
@@ -0,0 +1,29 @@
+
+
+<template>
+
+<!--SEZIONE BONUS CHE APPARE FINO AL CARICAMENTO COMPLETO DEGLI ELEMENTI-->
+
+  <div class="loading">
+      Loading...
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MyLoading",
+}
+</script>
+
+<style scoped lang="scss">
+
+  .loading {
+    font-size: 35px;
+    color: white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+</style>
